bea1ad5fddb81c201d33468c3fb7da4fd0fd8941
Zach Newton <znewton13@gmail.com>
2019-03-06 17:57:17 -0800

    Sidebar toggle animations

--- client/src/play/sidebar/Sidebar.tsx
+++ client/src/play/sidebar/Sidebar.tsx
@@ -1,25 +1,43 @@
 import * as React from 'react';
 import style from './Sidebar.st.css';
 
+type ListItemRenderer = (
+  item: any,
+  props?: { className: string; expanded: boolean }
+) => React.ReactElement;
+
 interface SidebarProps {
   title?: string;
   className?: string;
+  items?: Array<any>;
+  listItemRenderer?: ListItemRenderer;
 }
 
 export const Sidebar: React.SFC<SidebarProps> = ({
   title = '',
   className = '',
+  items = [1, 2, 3],
+  listItemRenderer = item => <li key={item}>{item}</li>,
 }: SidebarProps) => {
   const [expanded, setExpanded] = React.useState(false);
   return (
     <div {...style('root', { expanded }, { className })}>
-      <button
-        {...style('toggle', { expanded }, {})}
-        onClick={() => setExpanded(!expanded)}
-      >
-        <span {...style('toggleArrows', { expanded }, {})} />
-        <span {...style('toggleTitle', { expanded }, {})}>{title}</span>
-      </button>
+      <div {...style('contentWrapper', { expanded }, {})}>
+        <div {...style('toggleWrapper', { expanded }, {})}>
+          <button
+            {...style('toggleButton', { expanded }, {})}
+            onClick={() => setExpanded(!expanded)}
+          >
+            <span {...style('toggleArrows', { expanded }, {})} />
+            <span {...style('toggleTitle', { expanded }, {})}>{title}</span>
+          </button>
+        </div>
+        <ul className={style.itemList}>
+          {items.map(item =>
+            listItemRenderer(item, { expanded, className: style.item })
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
