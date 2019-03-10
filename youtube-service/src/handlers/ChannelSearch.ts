6d0ac2573ca8d30ed242760629a3b341170474d2
Zach Newton <znewton13@gmail.com>
2019-03-09 16:50:43 -0800

    Types in ChannelSearch

--- youtube-service/src/handlers/ChannelSearch.ts
+++ youtube-service/src/handlers/ChannelSearch.ts
@@ -1,13 +1,47 @@
 import { Request, Response } from 'express';
+import { youtube_v3 } from 'googleapis';
 import Handler from './Handler';
 
 import yt from '../lib/yt';
 
-interface ChannelResponse {
+interface Thumbnail {
+  url?: string;
+  width?: number;
+  height?: number;
+}
+
+interface Channel {
+  id: string;
+  title: string;
+  description: string;
+  thumbnails: Map<string, Thumbnail>;
+}
+
+interface ChannelListResponse {
   status: number;
-  channels: Array<any>;
+  channels: Array<Channel | undefined>;
   query: string;
   error?: any;
+}
+
+function channelSnippetToChannel(
+  snippet: youtube_v3.Schema$SearchResultSnippet
+): Channel {
+  const { channelId, title, description, thumbnails } = snippet;
+
+  const c: Channel = {
+    id: channelId || '',
+    title: title || '',
+    description: description || '',
+    thumbnails: Object.entries(thumbnails || {}).reduce((acc, [key, val]) => {
+      return {
+        ...acc,
+        [key]: val,
+      };
+    }, new Map()),
+  };
+
+  return c;
 }
 
 export default class ChannelSearch extends Handler {
@@ -15,7 +49,7 @@ export default class ChannelSearch extends Handler {
 
   async handle(req: Request, res: Response) {
     const query: string = req.param('query');
-    const channelResponse: ChannelResponse = {
+    const channelResponse: ChannelListResponse = {
       status: 200,
       query: query,
       channels: [],
@@ -24,12 +58,19 @@ export default class ChannelSearch extends Handler {
       return res.json(channelResponse);
     }
     try {
-      const channels = await yt.search.list({
+      const channelSearchResult = await yt.search.list({
         part: 'snippet',
         type: 'channel',
         q: query,
       });
-      channelResponse.channels = channels.data.items || [];
+      if (channelSearchResult.data.items) {
+        channelResponse.channels = channelSearchResult.data.items.map(
+          channel => {
+            if (channel && channel.snippet)
+              return channelSnippetToChannel(channel.snippet);
+          }
+        );
+      }
     } catch (e) {
       channelResponse.status = 500;
       channelResponse.error = e;
